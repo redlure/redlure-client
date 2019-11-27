ab129c22f71ed41ca2583c7b4ed98d295f7a7e51
Tw1sm <mcreel31@gmail.com>
2019-11-27 09:46:52 -0500

    added datatable

--- src/app/domains-servers/server-processes/server-processes.component.ts
+++ src/app/domains-servers/server-processes/server-processes.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy, AfterViewInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
+import { DataTableDirective } from 'angular-datatables';
 
 
 @Component({
   selector: 'app-server-processes',
   templateUrl: './server-processes.component.html'
 })
-export class ServerProcessesComponent implements OnInit {
+export class ServerProcessesComponent implements OnInit, AfterViewInit, OnDestroy {
   alias: String;
   processes: Object[];
+
+  dtTrigger: Subject<any> = new Subject();
+  dtOptions: DataTables.Settings = {};
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
 
   headers = ["Protocol", "Local address", "Status", "PID", "Program name"]
 
@@ -22,5 +29,14 @@ export class ServerProcessesComponent implements OnInit {
   closeModal() {
     this.activeModal.close();
   }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    this.dtTrigger.next()
+  }
+  
 
 }

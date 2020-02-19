dbfa930c5b67ae38bdb54c108418c8df5feca761
Tw1sm <mcreel31@gmail.com>
2020-02-19 14:20:53 -0500

    navbar edits

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,8 +7,7 @@ import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'main-navbar',
-  templateUrl: './navbar.component.html',
-  //styleUrls: ['./navbar.component.css']
+  templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
 
